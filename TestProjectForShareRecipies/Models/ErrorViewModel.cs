0a54802fde8aaf98156fbffa7760c02f0d28747b
Ivan Aleksandrov <ivan.t.aleksandrov@gmail.com>
2023-07-07 21:22:13 +0300

    Add project files.

---
+++ TestProjectForShareRecipies/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestProjectForShareRecipies.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
